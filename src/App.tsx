f2e99c9420dd25bb347508a54938a82c2aae95a9
Thomas VINCENT <thomas.vincent@esrf.fr>
2021-04-29 16:11:52 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-
 import ndarray from 'ndarray';
-import fromArrayBuffer from 'numpy-parser';
-import axios from 'axios';
-import { VisCanvas, PanZoomMesh, HeatmapMesh, ScaleType, ColorBar, ColorMap, Domain } from '@h5web/lib';
+import { fromArrayBuffer } from 'numpy-parser';
+import axios, { AxiosInstance } from 'axios';
+import {
+  VisCanvas,
+  PanZoomMesh,
+  HeatmapMesh,
+  ScaleType,
+  ColorBar,
+  ColorMap,
+  Domain,
+} from '@h5web/lib';
 import { useEffect, useState } from 'react';
 import './App.css';
+import Slider from '@material-ui/core/Slider';
+
+class H5DataProvider {
+  public readonly filepath: string;
+  protected readonly client: AxiosInstance;
+
+  public constructor(url: string, filepath: string) {
+    this.filepath = filepath;
+    this.client = axios.create({
+      baseURL: `${url}/hdf`,
+    });
+  }
+
+  public async getValue(path: string, selection?: string): Promise<unknown> {
+    const { data } = await this.client.get<unknown>(
+      `/data/${this.filepath}?uri=${path}${selection && `&ixstr=${selection}`}`
+    );
+    console.log(data);
+    // const { data, shape } = fromArrayBuffer(response);
+    // return ndarray(data, shape);
+    return 1;
+  }
+}
 
 interface Props {
   dataArray: ndarray;
@@ -21,17 +51,17 @@ function SliceVis(props: Props) {
     domain,
     title,
     scaleType = ScaleType.Linear,
-    colorMap = "Viridis",
+    colorMap = 'Viridis',
     invertColorMap = false,
   } = props;
 
-  const [rows, cols] = dataArray.shape
+  const [rows, cols] = dataArray.shape;
   const abscissaConfig = {
-    domain: [0, rows-1] as Domain,
+    domain: [0, rows - 1] as Domain,
     showGrid: false,
   };
   const ordinateConfig = {
-    domain: [0, cols-1] as Domain,
+    domain: [0, cols - 1] as Domain,
     showGrid: false,
   };
 
@@ -39,9 +69,9 @@ function SliceVis(props: Props) {
     <VisCanvas
       abscissaConfig={abscissaConfig}
       ordinateConfig={ordinateConfig}
-      aspectRatio={1}
+      // aspectRatio={1}
       canvasTitle={title}
-      >
+    >
       <PanZoomMesh />
       <HeatmapMesh
         rows={rows}
@@ -56,90 +86,196 @@ function SliceVis(props: Props) {
   );
 }
 
-
 // numpy like
-function npRandom(shape: number[]) : ndarray {
-  const size = shape.reduce((accumulator, currentValue) => accumulator * currentValue);
-  let array = new Float32Array(size);
-  for (var i: number=0; i<size; i++) {
+function npRandom(shape: number[]): ndarray {
+  const size = shape.reduce(
+    (accumulator, currentValue) => accumulator * currentValue
+  );
+  const array = new Float32Array(size);
+  for (let i = 0; i < size; i++) {
     array[i] = Math.random();
   }
   return ndarray(array, shape);
 }
 
 // download
-async function npyFetch(url: string) : Promise<ndarray> {
+
+async function npyFetch(url: string): Promise<ndarray> {
   const response = await fetch(url);
   const arrayBuffer = await response.arrayBuffer();
   const { data, shape } = fromArrayBuffer(arrayBuffer);
   return ndarray(data, shape);
 }
 
-//Data hook
-const dataArray = npRandom([2048, 2048]);
-const domain: Domain = [0, 1];
+/*
+const imageURL =
+  'http://localhost:5000/npy/data/public/image.npy?ixstr=100:200,:50';
+const image2URL = 'http://localhost:5000/npy/data/public/image2.npy';
+*/
 
-const dataArray2 = npRandom([2048, 2048]);
-const domain2: Domain = [0, 1];
+const h5file = 'public/data/BM18/MRI3D.h5';
+const h5path = '/data_128';
+const rootURL = 'http://localhost:5000/';
+const dataURL = rootURL + '/hdf/data/';
+const metaURL = rootURL + '/hdf/meta/';
+const statsURL = rootURL + 'hdf/stats/';
 
+// Data hook
+/*
 function useData(timeout: number): [ndarray, Domain] {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   useEffect(() => {
-    const interval = setInterval(() => {setCount(c => c + 1)}, timeout);
+    const interval = setInterval(() => {
+      setCount((c) => c + 1);
+    }, timeout);
     return () => clearInterval(interval);
   }, [timeout]);
   return count % 2 === 0 ? [dataArray, domain] : [dataArray2, domain2];
 }
+*/
 
-const imageURL = "http://localhost:3000/image.npy";
-axios.get(imageURL).then((response) => {
-  console.log(response.data);
-  npLoad(response.data);
-  const { data, shape } = fromArrayBuffer(response.data);
-}).catch((error) =>{
-  console.log(error);
-})
+function useSliceData(
+  h5file: string,
+  h5path: string,
+  dim: number
+): [ndarray, Domain, (newValue: number) => void] {
+  const [slice, setSlice] = useState(-1);
+  const [data, setData] = useState(ndarray(new Float32Array(0), [0, 0]));
+  const [domain, setDomain] = useState([0, 1] as Domain);
+  function setNewSlice(newValue: number) {
+    if (newValue !== slice) {
+      setSlice(newValue);
+      const ixstr = `${':,'.repeat(dim)}${newValue}`;
+      const currentDataURL = `${dataURL}${h5file}?uri=${h5path}&ixstr=${ixstr}`;
+      const currentStatsURL = `${statsURL}${h5file}?uri=${h5path}&ixstr=${ixstr}`;
+      Promise.all([npyFetch(currentDataURL), axios.get(currentStatsURL)]).then(
+        (values) => {
+          setData(values[0]);
+          setDomain([values[1].data.nanmin, values[1].data.nanmax]);
+        }
+      );
+    }
+  }
+  return [data, domain, setNewSlice];
+}
 
 function App() {
-  const [data, domain] = useData(1000);
   const scaleType = ScaleType.Linear;
-  const colorMap = "Viridis";
+  const colorMap = 'Greys';
+  const invertColorMap = true;
+  const axialDim = 0;
+  const frontDim = 1;
+  const sideDim = 2;
+
+  const [domain, setDomain] = useState([0, 1] as Domain);
+
+  const [dataAxial, domainAxial, setNewSliceAxial] = useSliceData(
+    h5file,
+    h5path,
+    axialDim
+  );
+  const [dataFront, domainFront, setNewSliceFront] = useSliceData(
+    h5file,
+    h5path,
+    frontDim
+  );
+  const [dataSide, domainSide, setNewSliceSide] = useSliceData(
+    h5file,
+    h5path,
+    sideDim
+  );
+
+  const [shape, setShape] = useState([0, 0, 0]);
+  useEffect(() => {
+    if (shape[0] === 0) {
+      Promise.all([
+        axios.get(`${metaURL}${h5file}?uri=${h5path}`),
+        axios.get(`${statsURL}${h5file}?uri=${h5path}`),
+      ]).then((values) => {
+        const shape = values[0].data.shape;
+        setShape(shape);
+        setNewSliceAxial(Math.round(shape[0] / 2));
+        setNewSliceSide(Math.round(shape[1] / 2));
+        setNewSliceFront(Math.round(shape[2] / 2));
+        setDomain([values[1].data.nanmin, values[1].data.nanmax] as Domain);
+      });
+    }
+  });
 
   return (
     <div className="App">
-      <SliceVis
-        dataArray={data}
-        domain={domain}
-        title="Axial"
-        scaleType={scaleType}
-        colorMap={colorMap}
-        invertColorMap={false}
-      />
       <div className="Tools">
-        <ColorBar
-          domain={domain as Domain}
+        <Slider
+          orientation="vertical"
+          defaultValue={0}
+          aria-labelledby="axial-slider"
+          valueLabelDisplay="on"
+          step={1}
+          marks
+          min={0}
+          max={shape[axialDim] - 1}
+          onChange={(event, newValue) => setNewSliceAxial(newValue as number)}
+        />
+        <SliceVis
+          dataArray={dataAxial}
+          domain={domain}
+          title="Axial"
           scaleType={scaleType}
           colorMap={colorMap}
-          invertColorMap={false}
+          invertColorMap={invertColorMap}
+        />
+      </div>
+      <div className="Tools">
+        <ColorBar
+          domain={domain}
+          scaleType={scaleType}
+          colorMap={colorMap}
+          invertColorMap={invertColorMap}
           withBounds
         />
       </div>
-      <SliceVis
-        dataArray={dataArray}
-        domain={domain as Domain}
-        title="Front"
-        scaleType={scaleType}
-        colorMap={colorMap}
-        invertColorMap={false}
-      />
-      <SliceVis
-        dataArray={dataArray}
-        domain={domain as Domain}
-        title="Side"
-        scaleType={scaleType}
-        colorMap={colorMap}
-        invertColorMap={false}
-      />
+      <div className="Tools">
+        <Slider
+          orientation="vertical"
+          defaultValue={0}
+          aria-labelledby="front-slider"
+          valueLabelDisplay="on"
+          step={1}
+          marks
+          min={0}
+          max={shape[frontDim] - 1}
+          onChange={(event, newValue) => setNewSliceFront(newValue as number)}
+        />
+        <SliceVis
+          dataArray={dataFront}
+          domain={domain}
+          title="Front"
+          scaleType={scaleType}
+          colorMap={colorMap}
+          invertColorMap={invertColorMap}
+        />
+      </div>
+      <div className="Tools">
+        <SliceVis
+          dataArray={dataSide}
+          domain={domain}
+          title="Side"
+          scaleType={scaleType}
+          colorMap={colorMap}
+          invertColorMap={invertColorMap}
+        />
+        <Slider
+          orientation="vertical"
+          defaultValue={0}
+          aria-labelledby="side-slider"
+          valueLabelDisplay="on"
+          step={1}
+          marks
+          min={0}
+          max={shape[sideDim] - 1}
+          onChange={(event, newValue) => setNewSliceSide(newValue as number)}
+        />
+      </div>
     </div>
   );
 }
